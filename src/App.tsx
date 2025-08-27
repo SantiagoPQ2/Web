40c19fe3fcda4cab6ce1a2dcb3aaa84413ae633c
SantiagoPQ2 <santiago@ibarazi.com.ar>
2025-08-27 18:01:28 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -28,8 +28,8 @@ function App() {
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex items-center justify-between h-16">
             <div className="flex items-center">
-              <div className="bg-blue-600 rounded-lg p-2 mr-3">
-                <FileSpreadsheet className="h-6 w-6 text-white" />
+              <div className="mr-3">
+                <img src="/image.png" alt="VaFood Logo" className="h-10 w-10 rounded" />
               </div>
               <div>
                 <h1 className="text-xl font-bold text-gray-900">
