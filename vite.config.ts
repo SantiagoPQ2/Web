f4cf210a174e963aafaa84a8914f7440336a3f37
SantiagoPQ2 <santiago@ibarazi.com.ar>
2025-09-01 13:32:38 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-});
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate', // 👈 clave
+      workbox: {
+        clientsClaim: true,
+        skipWaiting: true,
+      },
+      devOptions: {
+        enabled: true
+      }
+    })
+  ]
+})
