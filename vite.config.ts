768ddefda745850d76c077ee4b7bb9cd54747ecd
SantiagoPQ2 <santiago@ibarazi.com.ar>
2026-03-12 12:15:23 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      registerType: 'autoUpdate',
-      workbox: {
-        clientsClaim: true,
-        skipWaiting: true
-      },
+      strategies: "injectManifest",
+      srcDir: "src",
+      filename: "sw.ts",
+      registerType: "autoUpdate",
       devOptions: {
-        enabled: true
+        enabled: true,
       },
-      manifest: false // usamos el manifest.json que está en /public
-    })
-  ]
-})
+      manifest: false, // seguimos usando el manifest.json de /public
+    }),
+  ],
+});
