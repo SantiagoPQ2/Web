fdfaa59a9070d150e4ce31eca0668a7a64492588
SantiagoPQ2 <santiago@ibarazi.com.ar>
2026-03-12 12:15:52 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,13 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-// Activar actualización automática del PWA
-import { enableAutoUpdate } from './pwa-update';
+import { enableAutoUpdate } from "./pwa-update";
+
 enableAutoUpdate();
 
-// IMPORTANTE:
-// No registrar manualmente el service worker aquí.
-// VitePWA lo maneja automáticamente gracias a:
-// registerType: "autoUpdate"
-
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
   </StrictMode>
